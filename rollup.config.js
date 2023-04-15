35e080bd2c81050920e03ded29e7807d8dfbbe23
Bertalan Kis <kbertalan@gmail.com>
2023-04-15 21:04:58 +0200

    remove rollup related files as it is no longer used from Makefile

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { terser } from "rollup-plugin-terser";
-
-export default {
-  input: "build/exec/tyttp",
-  output: [
-    { file: "build/dist/tyttp.js", format: "cjs" },
-    { file: "build/dist/tyttp.min.js", format: "cjs", plugins: [terser()] }
-  ],
-};
